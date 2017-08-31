bd84e3fb03717686ed0f51c445238cff785826b8
Ty Richard <tyrichard2@icloud.com>
2017-08-30 18:47:54 -0600

    toolbar functionality

--- app/toolbar/toolbar_controller.js
+++ app/toolbar/toolbar_controller.js
@@ -1,9 +1,19 @@
 (function() {
-    'use strict';
-    angular.module('angular-inbox', )
-        .controller('toolbarController', toolbarController)
+        'use strict';
+        angular.module('angular-inbox', )
+            .controller('toolbarController', toolbarController)
 
-    function toolbarController() {
-        console.log("toolbar")
+        function toolbarController() {
+            const vm = this;
+            vm.toggleAll = function(message) {
+                var toggleStatus = !$scope.isAllSelected;
+                angular.forEach($scope.options, function(itm) { itm.selected = toggleStatus; });
+
+            }
+
+            $scope.optionToggled = function() {
+                $scope.isAllSelected = $scope.options.every(function(itm) { return itm.selected; })
+            }
+        }
     }
-}());
+    ());
