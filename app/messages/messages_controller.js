40268ac4ba96899f21c7273e1495cd0033777245
Ty Richard <tyrichard2@icloud.com>
2017-09-08 21:37:43 -0600

    api integration

--- app/messages/messages_controller.js
+++ app/messages/messages_controller.js
@@ -3,14 +3,23 @@
         angular.module('angular-inbox', )
             .controller('messagesController', messagesController)
 
-        function messagesController() {
+        function messagesController($http) {
             const vm = this;
+
             vm.toggleStar = function(messages) {
-                messages.starred = !messages.starred
+                const url = "https://salty-inlet-68026.herokuapp.com/api"
+                var body = {
+                    messageIds: [messages.id],
+                    command: "star",
+                    star: !messages.starred
+                }
+                $http.patch(url + "/messages", JSON.stringify(body)).then(function(res) {
+                    messages.starred = !messages.starred
+                })
+
             };
             vm.checkedMessage = function(selected, messages) {
                 messages.selected = selected
-                console.log(messages)
             };
         }
     }
